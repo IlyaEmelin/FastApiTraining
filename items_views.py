3103a84486860316d242fe3e2845978420b1a69b
Илья <bkmz8_133@mail.ru>
2025-03-19 15:20:22 +0300

    install aiosqlite

--- items_views.py
+++ items_views.py
@@ -2,32 +2,19 @@ from typing import Annotated
 
 from fastapi import APIRouter, Path
 
-router = APIRouter(prefix='/items', tags= ['Items'])
+router = APIRouter(prefix="/items", tags=["Items"])
 
-@router.get('/')
+
+@router.get("/")
 def list_items():
-    return [
-        'Item1',
-        'Item2',
-        'Item3',
-        'Item4'
-    ]
+    return ["Item1", "Item2", "Item3", "Item4"]
 
 
-@router.get('/latest/')
+@router.get("/latest/")
 def get_latest_item():
-    return {
-        'item': {
-            'id': "0",
-            'name': 'latest'
-        }
-    }
+    return {"item": {"id": "0", "name": "latest"}}
 
 
-@router.get('/{item_id}/')
+@router.get("/{item_id}/")
 def get_item_by_id(item_id: Annotated[int, Path(ge=1, lt=1_000_000)]):
-    return {
-        'item': {
-            'id': item_id
-        }
-    }
+    return {"item": {"id": item_id}}
